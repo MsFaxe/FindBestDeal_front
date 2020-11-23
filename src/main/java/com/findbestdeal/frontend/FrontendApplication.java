413dcdf0e18fafb74701186be2aacc04842a2040
MsFaxe <anty.wjj@gmail.com>
2020-11-23 14:30:46 +0100

    update .gitignore & build.gradle

--- src/main/java/com/findbestdeal/frontend/FrontendApplication.java
+++ src/main/java/com/findbestdeal/frontend/FrontendApplication.java
@@ -1,9 +1,11 @@
 package com.findbestdeal.frontend;
 
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableVaadin
 public class FrontendApplication {
 
     public static void main(String[] args) {
