a105d84fbda1012419601ff67ee22a8192bfe21c
MsFaxe <anty.wjj@gmail.com>
2020-11-23 13:36:50 +0100

    add basic Mainview

---
+++ src/main/java/com/findbestdeal/frontend/view/MainView.java
@@ -0,0 +1,13 @@
+package com.findbestdeal.frontend.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Button("SEARCH", e -> Notification.show("test game")));
+    }
+}
